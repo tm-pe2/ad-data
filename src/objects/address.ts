da60c0d62331b6bb7808960aa3a1ed7df4765090
Endri <net1gaming2@gmail.com>
2022-06-10 16:53:40 +0200

    Get address from csv file and create objects

--- src/objects/address.ts
+++ src/objects/address.ts
@@ -1,6 +1,27 @@
-import { cities } from '../env/cities_postal_codes';
 import { ICityPostCode } from '../models/address';
+import * as fs from "fs";
+import * as path from "path";
 
-export const addAddresses = async () => {
+const cities: ICityPostCode[] = [];
+const filePath = path.resolve(__dirname, '../env/belgian-cities.csv');
+const data = fs.readFileSync(filePath, { encoding: 'utf-8' }).split('\n');
+
+const parseCities = async () => {
+    
+    for(let i = 0; i < data.length; i++)
+    {
+        const row = data[i].split(',');
+        const tempCity: ICityPostCode = {
+            postal_code: row[0],
+            city_name: row[1]
+        }
+        cities.push(tempCity);
+    }
+}
+
+
+export const addAddresses = async () => { 
+    await parseCities();
+    console.log(cities);
     //do call to api
 }
